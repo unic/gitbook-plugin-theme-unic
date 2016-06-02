4c817dcd33d92ea7ae31d08fe94970e803c56705
Reto Hugi <reto.hugi>
2016-06-02 18:06:04 +0200

    add copyright info

--- book/unic.js
+++ book/unic.js
@@ -1,3 +1,7 @@
+/**  
+ * Copyright [YEAR] Unic AG
+ */
+
 require(["gitbook", "jQuery"], function(gitbook, $) {
     gitbook.events.bind("page.change", function(e, config) {
         $(".book-header .pull-left").last().before($("<div class='unic-logo'><img src='http://www.unic.com/img/unic-logo.png' /></div>"));
